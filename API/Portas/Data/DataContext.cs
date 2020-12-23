d7004db0760e0ed868d02413e512b984af6193e0
lockade <jeffersondias-@hotmail.com>
2020-12-22 23:11:42 -0300

    Final

---
+++ API/Portas/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portas.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> TBUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(b => b.email)
+                .IsUnique();
+        }
+    }
+}
